c39054000f7752107de9e8013ea21f506f5766d5
lnx <a16725798566@163.com>
2026-05-02 11:11:03 +0800

    8.2 添加敌人的不同状态以及状态的切换函数

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -6,21 +6,66 @@
 void Enemy::init()
 {
     Actor::init();
-    auto path = game_.getConfig()->get<std::string>("enemy.sprite_dead", "assets/sprite/ghostDead-Sheet.png");
-    SpriteAnim::addSpriteAnimChild(this, "assets/sprite/ghost-Sheet.png", 2.0f);
+    anim_normal_ = SpriteAnim::addSpriteAnimChild(this, game_.getConfig()->get<std::string>("enemy.sprite_move"), 2.0f);
+    anim_hurt_ = SpriteAnim::addSpriteAnimChild(this, game_.getConfig()->get<std::string>("enemy.sprite_hurt"), 2.0f);
+    anim_die_ = SpriteAnim::addSpriteAnimChild(this, game_.getConfig()->get<std::string>("enemy.sprite_dead"), 2.0f);
+    anim_hurt_->setActive(false);
+    anim_die_->setActive(false);
+    current_anim_ = anim_normal_;
 }
 
-void Enemy::update(float dt){
+void Enemy::update(float dt)
+{
     Actor::update(dt);
     aim_target(target_);
     move(dt);
+    timer_ += dt;
+    if (timer_ > 2.0f && timer_ < 4.0f)
+    {
+        changeState(State::HURT);
+    }
+    else if (timer_ > 4.0f)
+    {
+        changeState(State::DIE);
+    }
+    if (timer_ > 6.0f){
+        timer_ = 0.0f;
+        changeState(State::NORMAL);
+    }
 }
 
 void Enemy::aim_target(Player *target)
 {
-    if (target == nullptr) return;
+    if (target == nullptr)
+        return;
     auto direction = target->getPosition() - this->getPosition();
     direction = glm::normalize(direction);
     velocity_ = direction * max_speed_;
 }
 
+void Enemy::changeState(State new_state)
+{
+    if (new_state == current_state_)
+        return;
+    current_anim_->setActive(false);
+    switch (new_state)
+    {
+    case State::NORMAL:
+        current_anim_ = anim_normal_;
+        current_anim_->setActive(true);
+        break;
+    case State::HURT:
+        current_anim_ = anim_hurt_;
+        current_anim_->setActive(true);
+        break;
+    case State::DIE:
+        current_anim_ = anim_die_;
+        current_anim_->setActive(true);
+        break;
+    }
+    current_state_ = new_state;
+}
+
+void Enemy::updateState(float dt)
+{
+}
--- src/enemy.h
+++ src/enemy.h
@@ -5,21 +5,42 @@
 #define ENEMY_H
 #include "core/actor.h"
 
-
 class Player;
 class Enemy : public Actor
 {
-    Player* target_ = nullptr;
+    enum class State
+    {
+        NORMAL,
+        HURT,
+        DIE
+    };
+
+    // 精灵动画
+    SpriteAnim *anim_normal_ = nullptr;
+    SpriteAnim *anim_hurt_ = nullptr;
+    SpriteAnim *anim_die_ = nullptr;
+
+    SpriteAnim *current_anim_ = nullptr;
+
+    // 状态相关
+    float timer_ = 0.0f; // 用于演示状态切换的计时器
+    State current_state_ = State::NORMAL;
+    Player *target_ = nullptr;
 
 public:
     virtual void init();
-    virtual void update(float dt) override;  // override the update function from Actor
+    virtual void update(float dt) override; // override the update function from Actor
 
-    void aim_target(Player* target);
+    // 行为函数
+    void aim_target(Player *target);
+
+    void changeState(State new_state);
+
+    void updateState(float dt);
 
     // getters and setters
-    Player* get_target() { return target_; }
-    void set_target(Player* target) { target_ = target; }
+    Player *get_target() { return target_; }
+    void set_target(Player *target) { target_ = target; }
 };
 
 #endif // ENEMY_H
